b7ba377a0be363ff82835c947cf977804a1533ee
Emmanueloz <davidozuna84@hotmail.com>
2023-06-17 16:10:13 -0600

    Metodo para consultar las ventas con sus detalles implementado

--- models/DetallesVentas.php
+++ models/DetallesVentas.php
@@ -73,4 +73,52 @@ class DetallesVentas
             $consulta->close();
         }
     }
+
+    public static function consultarDetallesVentas($id = null)
+    {
+        $detallesVentaArray = [];
+        $idVenta = 0;
+        $productos = '';
+        $cantidad = 0;
+        $costo = 0;
+
+        if ($id == null) {
+
+
+
+            $consulta = self::$bd->prepare("SELECT detalles_venta.id_venta, GROUP_CONCAT(productos.numero_serie,':',productos.marca,' '),
+            detalles_venta.cantidad,detalles_venta.costo
+            FROM ventas, productos, detalles_venta
+            WHERE ventas.id_venta = detalles_venta.id_venta 
+            AND productos.id_producto = detalles_venta.id_producto
+            GROUP BY detalles_venta.id_venta");
+            $consulta->execute();
+            $consulta->bind_result($idVenta, $productos, $cantidad, $costo);
+
+            while ($consulta->fetch()) {
+
+                array_push($detallesVentaArray, new DetallesVentas($idVenta, $productos, $cantidad, $costo));
+            }
+
+            $consulta->close();
+            return  $detallesVentaArray;
+        } else {
+
+            $consulta = self::$bd->prepare("SELECT detalles_venta.id_venta, GROUP_CONCAT(productos.numero_serie,':',productos.marca,' '),
+            detalles_venta.cantidad,detalles_venta.costo
+            FROM ventas, productos, detalles_venta
+            WHERE ventas.id_venta = detalles_venta.id_venta 
+            AND productos.id_producto = detalles_venta.id_producto
+            AND detalles_venta.id_venta = ?
+            GROUP BY detalles_venta.id_venta");
+            $consulta->bind_param('i', $id);
+            $consulta->execute();
+            $consulta->store_result();
+            $consulta->bind_result($idVenta, $productos, $cantidad, $costo);
+            $consulta->fetch();
+            $consulta->close();
+
+            return new DetallesVentas($idVenta, $productos, $cantidad, $costo);
+        }
+    }
 }
--- models/Ventas.php
+++ models/Ventas.php
@@ -10,6 +10,7 @@ class Ventas
     public $idEmpleado;
     public $idCliente;
     public $fechaVenta;
+    public $detalleVenta;
 
     private static $bd;
 
@@ -34,7 +35,7 @@ class Ventas
         return  $idVenta;
     }
 
-    public function __construct($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta)
+    public function __construct($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta, $detalleVenta)
     {
         $this->idVenta = $idVenta;
         $this->subtotal = $subtotal;
@@ -42,6 +43,7 @@ class Ventas
         $this->idEmpleado = $idEmpleado;
         $this->idCliente = $idCliente;
         $this->fechaVenta = $fechaVenta;
+        $this->detalleVenta = $detalleVenta;
     }
 
     public function agregarVenta($idProductos, $cantidad, $costo)
@@ -62,6 +64,55 @@ class Ventas
         $detalleVenta = new DetallesVentas($idVenta, $idProductos, $cantidad, $costo);
         $detalleVenta->agregarDetalles();
     }
+
+    public static function consultarVentas($id = null)
+    {
+        $ventasArray = [];
+        $idVenta = 0;
+        $subtotal = 0;
+        $iva = 0;
+        $idEmpleado = 0;
+        $idCliente = 0;
+        $fechaVenta = '';
+
+        if ($id == null) {
+            $detallesVenta = DetallesVentas::consultarDetallesVentas();
+
+            $consulta = self::$bd->prepare("SELECT ventas.id_venta, ventas.subtotal,ventas.iva,ventas.empleados_id_empleado,ventas.clientes_id_cliente,
+            ventas.fecha_venta
+            FROM ventas,detalles_venta
+            WHERE ventas.id_venta = detalles_venta.id_venta
+            GROUP BY ventas.id_venta");
+            $consulta->execute();
+            $consulta->bind_result($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta);
+
+            $contador = 0;
+
+            while ($consulta->fetch()) {
+                array_push($ventasArray, new Ventas($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta, $detallesVenta[$contador]));
+                $contador++;
+            }
+
+            $consulta->close();
+            return  $ventasArray;
+        } else {
+
+            $consulta = self::$bd->prepare("SELECT ventas.id_venta, ventas.subtotal, ventas.iva, ventas.empleados_id_empleado, 
+            ventas.clientes_id_cliente, ventas.fecha_venta
+            FROM ventas
+            WHERE ventas.id_venta = ?");
+
+            $consulta->bind_param('i', $id);
+            $consulta->execute();
+            $consulta->store_result();
+            $consulta->bind_result($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta);
+            $consulta->fetch();
+            $consulta->close();
+            $detalleVenta = DetallesVentas::consultarDetallesVentas($idVenta);
+
+            return new Ventas($idVenta, $subtotal, $iva, $idEmpleado, $idCliente, $fechaVenta, $detalleVenta);
+        }
+    }
 }
 
 $mysqli = new mysqli("localhost", "root", "", "agenciaBD");
@@ -70,26 +121,29 @@ Ventas::init($mysqli);
 DetallesVentas::init($mysqli);
 
 $idProductos = [3, 1, 5];
+$cantidad = count($idProductos);
+
 $subtotal = DetallesVentas::obtenerSubtotal($idProductos);
 $iva = Ventas::generarIva($subtotal);
 $costo = DetallesVentas::calcularCosto($subtotal, $iva);
 
 $idVenta = Ventas::obtenerIdVenta();
-
 $fechaActual = date("Y-m-d");
+
 
 if (isset($argc) && $argc == 2) {
     switch ($argv[1]) {
         case 'nuevo':
-            $venta = new Ventas($idVenta, $subtotal, $iva, 5, 2, $fechaActual);
-            $venta->agregarVenta($idProductos, 1, $costo);
+            $venta = new Ventas($idVenta, $subtotal, $iva, 5, 2, $fechaActual, 0, 0, 0);
+            $venta->agregarVenta($idProductos, $cantidad, $costo);
             break;
         case 'consulta':
-            echo "Subtotal: $subtotal \n";
-            echo "Iva: $iva \n";
-            echo "Costo Total: $costo \n";
-            echo "Id venta: $idVenta \n";
-            echo  $fechaActual = date("Y-m-d") . "\n";
+            $ventas = Ventas::consultarVentas();
+            print_r($ventas);
+            break;
+        case 'detalles':
+            $detallesVenta = DetallesVentas::consultarDetallesVentas();
+            print_r($detallesVenta);
             break;
     }
 }
