5221b1c73ae12609f38bd3210c5308fcc17a1acf
Emmanueloz <davidozuna84@hotmail.com>
2023-06-23 15:09:18 -0600

    Vista agregar venta implementada con la lista de clientes y empleados real

--- vista_agregar_venta.php
+++ vista_agregar_venta.php
@@ -4,11 +4,32 @@ require_once 'models/config.php';
 require_once 'models/DetallesVentas.php';
 require_once 'models/Ventas.php';
 
+$mysqli = new mysqli($servidor, $usuario, $password, $bd);
+
+
+Ventas::init($mysqli);
+DetallesVentas::init($mysqli);
+Empleados::init($mysqli);
+Clientes::init($mysqli);
+Productos::init($mysqli);
+
 $titulo = "Agregar una nueva venta";
 $html = new SpynTPL('views/');
 $html->Fichero('agregar_ventas.html');
 $html->Asigna('title', $titulo);
 $html->Asigna('mensaje', ' ');
 
+$empleados = Empleados::consul();
 
+$clientes = Clientes::consulta();
+
+foreach ($empleados as $empleado) {
+    $html->AsignaBloque("empleados", $empleado);
+}
+
+foreach ($clientes as $cliente) {
+    $html->AsignaBloque("clientes", $cliente);
+}
+
+#print_r($clientes);
 echo $html->Muestra();
