ec8d15f909ad371ac81052e6385c726fe8271249
josuelpz <josuelpzcruz11@gmail.com>
2023-07-07 12:42:17 -0600

    generar reporte primeros pasos

--- report/reportClientes.php
+++ report/reportClientes.php
@@ -1,3 +1,22 @@
 <?php
 
-print_r($_GET);
+require('../fpdf/fpdf.php');
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Times');
+
+if(isset($_GET['value']) && !empty(trim($_GET['value'])))
+{
+    $pdf->Cell(180, 1, utf8_decode('Reporte de Consulta'), 0, 0, 'C');
+}
+elseif(isset($_GET['id_cliente']))
+{
+    $pdf->Cell(180, 1, utf8_decode('Reporte de Cliente'), 0, 0, 'C');
+}
+else
+{
+    $pdf->Cell(180, 1, utf8_decode('Reporte General'), 0, 0, 'C');
+}
+
+$pdf->Output();
