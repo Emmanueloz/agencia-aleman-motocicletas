641e30d738a8df73ed90ae065a826af1da079aee
josuelpz <josuelpzcruz11@gmail.com>
2023-07-09 14:50:18 -0600

    generar reporte pdf

--- report/reportClientes.php
+++ report/reportClientes.php
@@ -1,22 +1,72 @@
 <?php
 
 require('../fpdf/fpdf.php');
+require_once '../models/config.php';
+require_once '../models/Clientes.php';
+
+$mysqli = new mysqli($servidor, $usuario, $password, $bd);
+
+Clientes::init($mysqli);
+
+date_default_timezone_set('America/Mexico_City');
+$fecha = date("Y-m-d");
 
 $pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Times');
+$pdf->AddPage('H');
 
-if(isset($_GET['value']) && !empty(trim($_GET['value'])))
+$pdf->SetFont('Arial', 'B', 16);
+$pdf->Text(120, 12, "Reporte: Clientes");
+$pdf->Ln(5);
+$pdf->SetFont('Arial', '', 14);
+$pdf->Cell(20, 5, "Fecha del reporte: $fecha");
+$pdf->Ln(10);
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(30, 10, "Id Cliente", 1, 0, 'C');
+$pdf->Cell(45, 10, "RFC", 1, 0, 'C');
+$pdf->Cell(45, 10, "Nombre", 1, 0, 'C');
+$pdf->Cell(40, 10, utf8_decode("Dirección"), 1, 0, 'C');
+$pdf->Cell(35, 10, utf8_decode("Teléfono"), 1, 0, 'C');
+$pdf->Cell(55, 10, "Correo", 1, 0, 'C');
+$pdf->Cell(25, 10, utf8_decode("Género"), 1, 0, 'C');
+$pdf->Ln();
+
+if(isset($_GET['opcion']) && !empty(trim($_GET['value'])))
 {
-    $pdf->Cell(180, 1, utf8_decode('Reporte de Consulta'), 0, 0, 'C');
+   $opcion = $_GET['opcion'];
+   $value = $_GET['value'];
+   $clientesArray = Clientes::busquedafil($opcion, $value);
 }
 elseif(isset($_GET['id_cliente']))
 {
-    $pdf->Cell(180, 1, utf8_decode('Reporte de Cliente'), 0, 0, 'C');
+    $id = $_GET['id_cliente'];
+    $clientesArray = Clientes::busquedafil('idcli', $id);
 }
 else
 {
-    $pdf->Cell(180, 1, utf8_decode('Reporte General'), 0, 0, 'C');
+    $clientesArray = Clientes::consulta();
+}
+
+$pdf->SetFont('Arial', '', 10);
+
+$row = 0;
+foreach($clientesArray as $clientes)
+{
+    $row += 1;
+    if($row > 10)
+    {
+        $pdf->AddPage('H');
+        $row = 0;
+    }
+
+    $pdf->Cell(30, 10, $clientes->id_cliente, "B", 0, 'C');
+    $pdf->Cell(45, 10, utf8_decode($clientes->rfc), "B", 0, 'C');
+    $pdf->Cell(45, 10, $clientes->nombre, "B", 0, 'C');
+    $pdf->Cell(40, 10, utf8_decode($clientes->direccion), "B", 0, 'C');
+    $pdf->Cell(35, 10, $clientes->telefono, "B", 0, 'C');
+    $pdf->Cell(55, 10, utf8_decode($clientes->correo), "B", 0, 'C');
+    $pdf->Cell(25, 10, $clientes->genero, "B", 0, 'C');
+
+    $pdf->Ln();
 }
 
 $pdf->Output();
