ca526bfcaf3154c7d9cf469eb2fa8edd68b147d3
Emmanueloz <davidozuna84@hotmail.com>
2023-07-09 15:36:59 -0600

    Inicio de sesion implementado, formulario de incio de sesión, funcional en las paginas de ventas

--- confirmar_venta.php
+++ confirmar_venta.php
@@ -1,6 +1,10 @@
 <?php
-
-#print_r($_POST);
+// Comprobar si hay una sesión iniciada
+session_start();
+require_once 'models/Login.php';
+if (!isset($_SESSION['user'])) {
+    header('Location: ./index.html');
+}
 require_once 'SpynTPL.php';
 require_once 'models/config.php';
 require_once 'models/Ventas.php';
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'SpynTPL.php';
+require_once 'models/config.php';
+require_once 'models/Login.php';
+
+$msg = '';
+if (isset($_GET['msg'])) {
+  $content  = $_GET['msg'];
+  $msg = "<div class='border alert alert-danger alert-dismissible fade show'
+    role='alert'><button type='button' class='btn-close' data-bs-dismiss='alert'
+    aria-label='Close'></button>$content</div>";
+}
+
+if (isset($_POST['usuario'])) {
+
+  $mysqli = new mysqli($servidor, $usuario, $password, $bd);
+  Login::init($mysqli);
+
+  $usuario = $_POST['usuario'];
+  $pass = $_POST['pass'];
+  session_start(); # Si existe la inicia y si ya existe la abre
+  if ($user = Login::login($usuario, $pass)) {
+    $_SESSION['user'] = serialize($user); # Permite convertir un objeto a una cadena de caracteres
+    header('Location: vista_empleados.php');
+  } else {
+    unset($_SESSION['user']); # Liberar memoria que tenga guardada en la variable que se coloca.
+    header('Location: login.php?msg=Datos incorrectos');
+  }
+}
+
+$html = new SpynTPL('views/');
+$html->Fichero('login.html');
+$html->Asigna('msg', $msg);
+
+echo $html->Muestra();
