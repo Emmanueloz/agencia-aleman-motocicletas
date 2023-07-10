65126a0154d736c8adc8dee4f94917ae137a02cd
Emmanueloz <davidozuna84@hotmail.com>
2023-07-10 10:51:10 -0600

    Seguridad en reportes ventas

--- report/reportVentas.php
+++ report/reportVentas.php
@@ -1,4 +1,11 @@
 <?php
+// Comprobar si hay una sesión iniciada
+session_start();
+require_once 'models/Login.php';
+if (!isset($_SESSION['user'])) {
+    header('Location: ./index.html');
+}
+
 require('../models/PDF.php');
 require_once '../models/config.php';
 require_once '../models/Ventas.php';
@@ -7,9 +14,6 @@ $mysqli = new mysqli($servidor, $usuario, $password, $bd);
 
 // Iniciando parámetros
 Ventas::init($mysqli);
-Empleados::init($mysqli);
-Clientes::init($mysqli);
-Productos::init($mysqli);
 
 // Configurando fecha
 date_default_timezone_set('America/Mexico_City');
