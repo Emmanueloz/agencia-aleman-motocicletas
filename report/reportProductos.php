07a25914e515a6906c67cce07106ec079db901cd
YisusNJ <mraul7875@gmail.com>
2023-07-09 18:33:29 -0600

    encabezado en reporte productos

--- report/reportProductos.php
+++ report/reportProductos.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user'])) {
 }
 
 
-require('../fpdf/fpdf.php');
+require('../models/PDF.php');
 require('../models/config.php');
 require_once '../models/Productos.php';
 
@@ -19,11 +19,11 @@ Productos::init($mysqli);
 date_default_timezone_set('America/Mexico_City');
 $fecha = date("Y-m-d");
 
-$pdf = new FPDF();
+$pdf = new PDF();
 $pdf->AddPage('H');
 $pdf->SetFont('Arial', 'B', 16);
 
-$pdf->Text(120, 12, "Reporte: Productos");
+$pdf->Cell(272, 12, 'Reporte de Productos', 0, 1, 'C');
 $pdf->Ln(5);
 $pdf->SetFont('Arial', '', 14);
 $pdf->Cell(20, 5, "Fecha del reporte: $fecha");
