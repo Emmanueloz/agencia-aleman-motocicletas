6c69544b00d61385fac0cd92ef1be6e9db833a94
YisusNJ <mraul7875@gmail.com>
2023-08-05 14:27:17 -0600

    modificacion en reporte y formulario

---
+++ js/validacion.js
@@ -0,0 +1,13 @@
+function validarNumero() {
+  let precio = document.getElementById("precio");
+  let existencias = document.getElementById("existencias");
+
+  if (precio.value.length <= 10 && existencias.value.length <= 10) {
+    return true;
+  } else {
+    alert(
+      `El campo precio o existencias supera el valor maximo, solo coloca menos 10 digitos`
+    );
+    return false;
+  }
+}
--- report/reportProductos.php
+++ report/reportProductos.php
@@ -19,23 +19,25 @@ Productos::init($mysqli);
 date_default_timezone_set('America/Mexico_City');
 $fecha = date("Y-m-d");
 
+$tipo = isset($_GET['opcion']) ? 'Consulta' : (isset($_GET['Producos']) ? 'Individual' : 'General');
+
 $pdf = new PDF();
 $pdf->AddPage('H');
 $pdf->SetFont('Arial', 'B', 16);
 
 $pdf->Cell(272, 12, 'Reporte de Productos', 0, 1, 'C');
+$pdf->Cell(136, 10, "Tipo de reporte: $tipo", 1, 1);
 $pdf->Ln(5);
 $pdf->SetFont('Arial', '', 14);
 $pdf->Cell(20, 5, "Fecha del reporte: $fecha");
 $pdf->Ln(10);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(20, 5, "ID", 1, 0, 'C');
 $pdf->Cell(45, 5, "Numero de Serie", 1, 0, 'C');
 $pdf->Cell(50, 5, "Marca", 1, 0, 'C');
 $pdf->Cell(45, 5, "Modelo", 1, 0, 'C');
 $pdf->Cell(50, 5, "Descripcion", 1, 0, 'C');
-$pdf->Cell(30, 5, "Existencias", 1, 0, 'C');
-$pdf->Cell(30, 5, "Precio", 1, 0, 'C');
+$pdf->Cell(40, 5, "Existencias", 1, 0, 'C');
+$pdf->Cell(40, 5, "Precio", 1, 0, 'C');
 $pdf->Ln();
 
 
@@ -56,13 +58,12 @@ foreach ($productoArray as $productos) {
         $pdf->AddPage('H');
         $row = 0;
     }
-    $pdf->Cell(20, 10, $productos->id_producto, "B", 0, 'C');
     $pdf->Cell(45, 10, utf8_decode($productos->numero_serie), "B", 0, 'L');
     $pdf->Cell(50, 10, utf8_decode($productos->marca), "B", 0, 'L');
     $pdf->Cell(45, 10, utf8_decode($productos->modelo), "B", 0, 'L');
     $pdf->Cell(50, 10, utf8_decode($productos->descripcion), "B", 0, 'L');
-    $pdf->Cell(30, 10, utf8_decode($productos->existencias), "B", 0, 'C');
-    $pdf->Cell(30, 10, '$' . $productos->precio, "B", 0, 'R');
+    $pdf->Cell(40, 10, utf8_decode($productos->existencias), "B", 0, 'C');
+    $pdf->Cell(40, 10, '$' . $productos->precio, "B", 0, 'R');
 
     $pdf->Ln();
 }
