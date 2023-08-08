24681a6ddee7e600d60d303d5888262571110a81
Emmanueloz <davidozuna84@hotmail.com>
2023-08-07 18:32:23 -0600

    Comentarios en el archivo JS

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,13 @@
+/**
+ * Valida si los campos precio y existencia sea menor o igual a diez dígitos
+ * @returns boolean si es falso no se manda el formulario
+ */
 function validarNumero() {
+  // Se obtiene los inputs del html mediante el ID
   let precio = document.getElementById("precio");
   let existencias = document.getElementById("existencias");
 
+  // Se valida la condición si el precio o existencia en su valor y su tamaño sea menor o igual a 10
   if (precio.value.length <= 10 && existencias.value.length <= 10) {
     return true;
   } else {
