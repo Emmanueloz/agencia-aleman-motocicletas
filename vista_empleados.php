2c1a3ca2db3c8f7fc593b4508086799145766419
Emmanueloz <davidozuna84@hotmail.com>
2023-08-07 18:57:07 -0600

    Botones

--- vista_empleados.php
+++ vista_empleados.php
@@ -6,6 +6,9 @@ require_once 'models/Login.php';
 if (!isset($_SESSION['user'])) {
     header('Location: ./index.php');
 }
+
+require_once './public/elements.php';
+
 require_once 'SpynTPL.php';
 require_once 'models/config.php';
 require_once 'models/Empleados.php';
@@ -44,24 +47,7 @@ if (isset($_GET['value']) && !empty(trim($_GET['value']))) {
     $value = $_GET['value'];
     $html->Asigna('link_report', "reportEmpleados.php?opcion=$opcion&value=$value");
     $html->Asigna('reporte', 'Reporte de consulta');
-    $buttonFiltro = '<a href="./vista_empleados.php" type="button" class="btn btn-outline-secondary rounded-0">
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="16"
-  height="16"
-  fill="currentColor"
-  class="bi bi-x-circle"
-  viewBox="0 0 16 16"
->
-  <path
-    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-  ></path>
-  <path
-    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-  ></path>
-</svg>
-</a>';
-
+    $buttonFiltro = bntLimpiarFiltro();
 
     switch ($opcion) {
         case 'id':
