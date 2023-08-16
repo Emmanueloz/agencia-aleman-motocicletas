b019424d53711a11577ae19f2994d5c220165b94
Emmanueloz <davidozuna84@hotmail.com>
2023-08-15 20:10:27 -0600

    resolviendo

--- vista_clientes.php
+++ vista_clientes.php
@@ -6,13 +6,13 @@ if (!isset($_SESSION['user'])) {
     header('Location: ./index.php');
 }
 
+require_once './public/elementos.php';
 require_once 'SpynTPL.php';
 require_once 'models/config.php';
 require_once 'models/Clientes.php';
 
 $msg = '';
-if(isset($_GET['msg']))
-{
+if (isset($_GET['msg'])) {
     $contenido = $_GET['msg'];
     $msg = "<div class='alert alert-success alert-dismissible fade show'
     role='alert'><button type='button' class='btn-close'
@@ -37,21 +37,7 @@ if (isset($_GET['value']) && !empty(trim($_GET['value']))) {
     $opcion = $_GET['opcion'];
     $value = $_GET['value'];
 
-    $buttonFiltro = '<a href="./vista_clientes.php" type="button" class="btn btn-outline-secondary">
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-x-circle"
-    viewBox="0 0 16 16"
-    ><path
-        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-    <path
-        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-    ></path>
-    </svg>
-    </a>';
+    $buttonFiltro = bntLimpiarFiltro();
 
     $html->AsignaBloque('paginas', null);
     $html->Asigna('link_report', "reportClientes.php?opcion=$opcion&value=$value");
