693b26fee01ba513fd05be78baaceeb09bf77f70
Emmanueloz <davidozuna84@hotmail.com>
2023-08-17 12:00:22 -0600

    pedir la contraseña para modificarla

--- usuario.php
+++ usuario.php
@@ -8,25 +8,41 @@ if (!isset($_SESSION['user'])) {
 require_once 'models/elementos.php';
 require_once('SpynTPL.php');
 require_once('models/config.php');
+$html = new SpynTPL('views/');
+$html->Fichero('frmUsuario.html');
+$html->Asigna('title', 'Modificar Usuario');
 
+$nav = navBar('usuario');
+$html->Asigna('nav-bar', $nav);
+$html->Asigna('msg', '');
+$html->Asigna('d-confirm', '');
+$html->Asigna('d-modify', 'd-none');
+$mysqli = new mysqli($servidor, $usuario, $password, $bd);
+Login::init($mysqli);
+$usuario = unserialize($_SESSION['user']);
+$user = $usuario->user();
+$name = $usuario->name();
 $title = 'Modificar Usuario';
-$target = 'usuario.php';
 
-
-if (isset($_POST['user'])) {
+if (isset($_POST['accion']) && $_POST['accion'] == 'modificar') {
   print_r($_POST);
-} else {
-  $html = new SpynTPL('views/');
-  $html->Fichero('frmUsuario.html');
-  $html->Asigna('title', $title);
-  $html->Asigna('target', $target);
+} elseif (isset($_POST['accion']) && $_POST['accion'] == 'comprobar') {
+  $password = $_POST['pass'];
+  $userVerify = Login::login($user, $password);
+  if (isset($userVerify)) {
+    $html->Asigna('d-confirm', 'd-none');
+    $html->Asigna('d-modify', '');
 
-  $nav = navBar('usuario');
-  $html->Asigna('nav-bar', $nav);
-
-  $usuario = unserialize($_SESSION['user']);
-  $html->Asigna('user', $usuario->user());
-  $html->Asigna('name', $usuario->name());
-  $html->Asigna('password', $usuario->password());
-  echo $html->Muestra();
+    $html->Asigna('user', $user);
+    $html->Asigna('name', $name);
+    $html->Asigna('password', $password);
+    $html->Asigna('accion', 'agregar');
+  } else {
+    $content = "Contraseña Incorrecta";
+    $msgError = "<div class='alert alert-danger alert-dismissible fade show'
+    role='alert'><button type='button' class='btn-close'
+    data-bs-dismiss='alert' aria-label='Close'></button>$content</div>";
+    $html->Asigna('msg', $msgError);
+  }
 }
+echo $html->Muestra();
